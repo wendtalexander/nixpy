c6290358c8d6af70e0a52ca659a723daffcd782d
wendtalexander <wendtalexander@protonmail.com>
2025-05-17 11:56:19 +0200

    [virtual_data_array] wrapping in cls, formatting

--- nixio/virtual_data_array.py
+++ nixio/virtual_data_array.py
@@ -6,11 +6,12 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted under the terms of the BSD License. See
 # LICENSE file in the root of the Project.
+from nixio.data_array import DataArray
 from .data_set import DataSet
 from .entity import Entity
 
 
-class VirtualDataArray(Entity, DataSet):
+class VirtualDataArray(DataArray):
     def __init__(self, nixfile, nixparent, h5group):
         super(VirtualDataArray, self).__init__(nixfile, nixparent, h5group)
         self._sources = None
@@ -18,9 +19,9 @@ class VirtualDataArray(Entity, DataSet):
 
     @classmethod
     def create_new(cls, nixfile, nixparent, h5parent, layout, name, type_):
-        newentity = super(VirtualDataArray, cls).create_new( nixfile,
-                                                            nixparent,
-                                                            h5parent, name,
-                                                            type_)
+        newentity = Entity.create_new(nixfile, nixparent, h5parent, name,
+                                      type_)
+        newentity = cls(nixfile, nixparent, newentity._h5group)
         newentity._h5group.group.create_virtual_dataset("data", layout)
+
         return newentity
